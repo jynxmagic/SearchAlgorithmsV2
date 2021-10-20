88b98d583ffb1c1a880e77503c04e58099023e0c
jynxmagic <ccarr895@gmail.com>
2021-10-20 22:03:29 +0100

    qol fixes

--- bin/search.py
+++ bin/search.py
@@ -1,9 +1,7 @@
-from queue import LifoQueue, PriorityQueue, Queue
+from queue import LifoQueue, Queue
 from time import time
 
 import ray
-from ray import remote
-
 from funcs import is_goal, is_in_closed_set, manhattan_distance, uniform_sort
 from states import get_children_states
 
@@ -37,7 +35,7 @@ def start_search_threads(initial_state, target_state):
     }
 
 
-@remote
+@ray.remote
 def tree_breadth_first_solve(initial_state, target_state):
     fifo_queue = Queue()
     fifo_queue.put(initial_state)
@@ -60,7 +58,7 @@ def tree_breadth_first_solve(initial_state, target_state):
             fifo_queue.put(child_states)
 
 
-@remote
+@ray.remote
 def tree_depth_first_solve(initial_state, target_state):
     lifo_queue = LifoQueue()
     lifo_queue.put(initial_state)
@@ -81,7 +79,7 @@ def tree_depth_first_solve(initial_state, target_state):
             lifo_queue.put(child_states)
 
 
-@remote
+@ray.remote
 def graph_breadth_first_solve(initial_state, target_state):
     fifo_queue = Queue()
     fifo_queue.put(initial_state)
@@ -111,7 +109,7 @@ def graph_breadth_first_solve(initial_state, target_state):
                 fifo_queue.put(child_state)
 
 
-@remote
+@ray.remote
 def graph_depth_first_solve(initial_state, target_state):
     lifo_queue = LifoQueue()
     lifo_queue.put(initial_state)
@@ -141,7 +139,7 @@ def graph_depth_first_solve(initial_state, target_state):
                 lifo_queue.put(child_state)
 
 
-@remote
+@ray.remote
 def tree_uniform_cost_solve(initial_state, target_state):
     # 1. uniform cost will go round on a cheap circle... repetetively... (until cost of going closer to objective is less than
     # cost of running in a circle) in a TREE only. While not limited, this can still cause issues.
@@ -179,7 +177,7 @@ def tree_uniform_cost_solve(initial_state, target_state):
             prio_queue = uniform_sort(prio_queue)
 
 
-@remote
+@ray.remote
 def graph_uniform_cost_solve(initial_state, target_state):
     prio_queue = []
     prio_queue.append((0, initial_state))
@@ -188,7 +186,7 @@ def graph_uniform_cost_solve(initial_state, target_state):
 
     start_time = time()
     i = 0
-    while not prio_queue.__sizeof__ == 0:
+    while prio_queue.__sizeof__() != 0:
         i += 1
         prio, current_state = prio_queue.pop(0)
         closed_set.append(
@@ -219,14 +217,14 @@ def graph_uniform_cost_solve(initial_state, target_state):
                 prio_queue = uniform_sort(prio_queue)
 
 
-@remote
+@ray.remote
 def tree_astar_solve(initial_state, target_state):
     prio_queue = []
     prio_queue.append((0, initial_state))
 
     start_time = time()
     i = 0
-    while not prio_queue.__sizeof__ == 0:
+    while prio_queue.__sizeof__() != 0:
         i += 1
         prio, current_state = prio_queue.pop(0)
 
@@ -253,7 +251,7 @@ def tree_astar_solve(initial_state, target_state):
             prio_queue = uniform_sort(prio_queue)
 
 
-@remote
+@ray.remote
 def graph_astar_solve(initial_state, target_state):
     prio_queue = []
     prio_queue.append((0, initial_state))
@@ -262,9 +260,9 @@ def graph_astar_solve(initial_state, target_state):
 
     start_time = time()
     i = 0
-    while not prio_queue.__sizeof__ == 0:
+    while prio_queue.__sizeof__() != 0:
         i += 1
-        prio, current_state = prio_queue.pop(0)
+        _, current_state = prio_queue.pop(0)
         closed_set.append(
             [current_state["encoded_state"][0], current_state["encoded_state"][1]]
         )
